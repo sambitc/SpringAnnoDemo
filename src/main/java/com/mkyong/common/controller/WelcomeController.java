db6ea4b3b3fcffcdfea7893a2938d273c830a7cf
sambitchoudhury <sambitgudu@yahoo.co.in>
2014-05-07 14:35:27 +0530

    add property and setter and getter

--- src/main/java/com/mkyong/common/controller/WelcomeController.java
+++ src/main/java/com/mkyong/common/controller/WelcomeController.java
@@ -1,10 +1,14 @@
 package com.mkyong.common.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
 
 @Controller
 @RequestMapping("/welcome.htm")
